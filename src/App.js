be8392d6c7a962daf1ce68aaab1b0176ba6d52fb
Mohamed Yassine EL AZIZ <MYEA@Mohameds-MacBook-Air.local>
2020-04-04 19:57:55 +0100

    test commit 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './followMyLead.jpg';
 import './App.css';
 
 function App() {
@@ -7,17 +7,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Bienvenue sur la hotline pour Kenza. Just follow my lead.
+          Quel est votre problème ce soir?
+        </h2>
+      
+        <select id="issue">
+          <option value="peur">J'ai peur toute seule</option>
+          <option value="myea">J'ai envie de toi</option>
+          <option value="fromage">J'ai envie de fromage</option>
+          <option value="dragibus">J'ai envie de dragibus</option>
+          <option value="viande">Je n'aime pas la viande</option>
+        </select>
+
+        <h3>La solution a votre problème est:</h3>
+    
       </header>
     </div>
   );

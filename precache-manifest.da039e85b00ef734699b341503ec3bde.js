fd1535282a258ec0957794eca1bc4c91e100ac09
Mohamed Yassine EL AZIZ <MYEA@Mohameds-MacBook-Air.local>
2020-04-04 19:57:21 +0100

    Updates

---
+++ precache-manifest.da039e85b00ef734699b341503ec3bde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f7940897f331663c913242fb562047",
+    "url": "/hotline-kenza/index.html"
+  },
+  {
+    "revision": "4d90db8deca4913e4fe6",
+    "url": "/hotline-kenza/static/css/main.390d25af.chunk.css"
+  },
+  {
+    "revision": "81f5ea0c6181c8784ea1",
+    "url": "/hotline-kenza/static/js/2.3bef3601.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hotline-kenza/static/js/2.3bef3601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d90db8deca4913e4fe6",
+    "url": "/hotline-kenza/static/js/main.d69fcf47.chunk.js"
+  },
+  {
+    "revision": "ed529a73baf1679a38fe",
+    "url": "/hotline-kenza/static/js/runtime-main.a6a42f72.js"
+  },
+  {
+    "revision": "9f082d53d70dce2b3870e21af8b068ec",
+    "url": "/hotline-kenza/static/media/followMyLead.9f082d53.jpg"
+  }
+]);
